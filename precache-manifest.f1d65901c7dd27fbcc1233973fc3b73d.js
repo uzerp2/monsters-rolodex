246ef4abbfcb33b7814741c7568a5768715c9a62
uzerp2 <uzerp2apple@gmail.com>
2020-09-30 16:00:54 +0300

    Updates

---
+++ precache-manifest.f1d65901c7dd27fbcc1233973fc3b73d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8786ecbb92d1637de44b560554bcb2d7",
+    "url": "/monsters-relodex/index.html"
+  },
+  {
+    "revision": "9375e4df9572ff6816a0",
+    "url": "/monsters-relodex/static/css/main.4fc14014.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-relodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-relodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9375e4df9572ff6816a0",
+    "url": "/monsters-relodex/static/js/main.4e0a3d08.chunk.js"
+  },
+  {
+    "revision": "91e553b22bb3c44ffb98",
+    "url": "/monsters-relodex/static/js/runtime-main.e994c128.js"
+  }
+]);
